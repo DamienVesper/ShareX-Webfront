68d9cd4925be7149338e95d9c8801f7a0424040b
Damien Vesper <ldamienvesper@gmail.com>
2021-10-02 19:00:55 -0500

    home: init

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,6 @@ import {
 import '../public/assets/scss/index.scss';
 import 'bootstrap';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-
 import Home from './pages/Home';
 
 import Error404 from './pages/404';
@@ -20,7 +17,6 @@ class App extends React.Component {
     render = (): React.ReactNode => {
         return (
             <div id="app" className="text-light d-flex flex-column h-100">
-                <Header />
                 <Router>
                     <Switch>
                         {/* Landing Page */}
@@ -34,7 +30,6 @@ class App extends React.Component {
                         </Route>
                     </Switch>
                 </Router>
-                <Footer />
             </div>
         );
     }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-// import '../../public/assets/scss/pages/home.scss';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+import '../../public/assets/scss/pages/home.scss';
 
 class Home extends React.Component {
     render = (): React.ReactNode => {
         return (
             <main className="text-center container">
+                <br />
+                <br />
+                <br />
+                <br />
                 <h1 className="page-title mt-5">Welcome to the Alru ShareX!</h1>
                 <p className="desc mt-3">
                     It&apos;s like Lunus, but better...
                 </p>
+                <br />
+                <br />
+                <br />
+
+                <button className="btn btn-lg btn-primary btn-sign-in">Sign In</button>
             </main>
         );
     }
